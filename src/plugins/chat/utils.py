b201bb69f00f5ac2fd810c53bdbeee01d329c95f
lmst2 <wujiachen2016@gmail.com>
2025-04-11 18:27:48 +0100

    修复一些小报错，以及把半角改回全角，貌似其他地方又用了全角

--- src/plugins/chat/utils.py
+++ src/plugins/chat/utils.py
@@ -47,7 +47,7 @@ def is_mentioned_bot_in_message(message: MessageRecv) -> bool:
     is_mentioned = False
 
     # 判断是否被@
-    if re.search(f"@[\s\S]*?\(id:{global_config.BOT_QQ}\)", message.processed_plain_text):
+    if re.search(fr"@[\s\S]*?（id:{global_config.BOT_QQ}）", message.processed_plain_text):
         is_at = True
         is_mentioned = True
 
@@ -57,11 +57,11 @@ def is_mentioned_bot_in_message(message: MessageRecv) -> bool:
     else:
         if not is_mentioned:
             # 判断是否被回复
-            if re.match(f"回复[\s\S]*?\({global_config.BOT_QQ}\)的消息，说：", message.processed_plain_text):
+            if re.match(fr"回复[\s\S]*?\({global_config.BOT_QQ}\)的消息，说：", message.processed_plain_text):
                 is_mentioned = True
 
             # 判断内容中是否被提及
-            message_content = re.sub(r"\@[\s\S]*?\((\d+)\)", "", message.processed_plain_text)
+            message_content = re.sub(r"\@[\s\S]*?（(\d+)）", "", message.processed_plain_text)
             message_content = re.sub(r"回复[\s\S]*?\((\d+)\)的消息，说： ", "", message_content)
             for keyword in keywords:
                 if keyword in message_content:
