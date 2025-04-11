3b662cee65bbcfe63b3062a7bfe30b6214f4c834
lmst2 <wujiachen2016@gmail.com>
2025-04-11 15:26:23 +0100

    思考时降低意愿，思考结束恢复意愿，临时解决方案

--- src/plugins/willing/mode_dynamic.py
+++ src/plugins/willing/mode_dynamic.py
@@ -3,6 +3,7 @@ import random
 import time
 from typing import Dict
 from .willing_manager import BaseWillingManager
+from src.plugins.chat.chat_stream import ChatStream
 
 
 class DynamicWillingManager(BaseWillingManager):
@@ -233,7 +234,6 @@ class DynamicWillingManager(BaseWillingManager):
 
             self.chat_reply_willing[chat_id] = min(2.0, current_willing + willing_increase)
 
-
     def change_reply_willing_after_sent(self, message_id):
         """发送消息后提高聊天流的回复意愿"""
         stream = self.ongoing_messages[message_id].chat
@@ -244,7 +244,8 @@ class DynamicWillingManager(BaseWillingManager):
 
             # 思考完成后恢复意愿，但是总体还是减少0.4， 因为已经回复了
             self.chat_reply_willing[chat_id] = max(0.0, current_willing + 0.4)
-        
+    
+    
     async def bombing_buffer_message_handle(self, message_id):
         return await super().bombing_buffer_message_handle(message_id)
 
